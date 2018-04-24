76746aec52208bad84f6568c00c40e74c1de043f
sharonrub <sharon.rub88@gmail.com>
2018-04-24 16:26:40 +0300

    Third commit

--- Exericse/index.js
+++ Exericse/index.js
@@ -1,1 +1,39 @@
-console.log("OK, so is it a different branch?");
+var options = [
+    'hummus', //1
+    'hamutzim', //2
+    'salat', //4
+    'chips' //8
+];
+
+var bitwiseOptions = {};
+var pita = 9;
+
+function buildOptions() {
+
+    for (var i=0; i<options.length; i++){
+        bitwiseOptions[options[i]] = 1 << i;
+    }
+}
+buildOptions();
+console.log(bitwiseOptions);
+
+
+
+function is(obj, option) {
+    if ((obj & bitwiseOptions[option]) === bitwiseOptions[option]){
+        console.log("true");
+        return true;
+    }
+    else{
+        console.log("false");
+        return false;
+    }
+}
+
+function set(obj, option) {
+    if ((!!bitwiseOptions[option]) && !(is(obj, option))){
+        console.log("this executes");
+        return pita += bitwiseOptions[option];
+    }
+
+}
